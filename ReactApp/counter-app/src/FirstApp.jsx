b7472ad65583137623c3be6be36ba03f06a917cf
YoarihtM <imacedo@totalplay.corp>
2022-11-30 09:53:22 -0600

    Practica proptypes de react

--- ReactApp/counter-app/src/FirstApp.jsx
+++ ReactApp/counter-app/src/FirstApp.jsx
@@ -1,11 +1,22 @@
+import PropTypes from 'prop-types';
+
 export const FirstApp = ({ title, subTitle }) => {
 
-    // console.log(props);
-
     return (
+
         <>
             <h1>{ title }</h1>
             <p>subtitle enviado desde el componente padre: '{ subTitle }'</p>
         </>
     );
 };
+
+FirstApp.propTypes = {
+    title : PropTypes.string.isRequired,
+    subTitle : PropTypes.number 
+}
+
+FirstApp.defaultProps = {
+    title : 'No hay titulo',
+    subTitle : 'No hay subtitulo'
+}
--- ReactApp/counter-app/src/index.jsx
+++ ReactApp/counter-app/src/index.jsx
@@ -10,7 +10,7 @@ import './styles.css';
 ReactDOM.createRoot( document.getElementById('root') ).render(
     <React.StrictMode>
         <HelloWorldApp />
-        <FirstApp title="Hola, Soy Goku" subTitle="123"/>
+        <FirstApp />
         <Fragmento />
     </React.StrictMode>
 )
